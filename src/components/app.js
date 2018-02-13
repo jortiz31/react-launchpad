72cebbe0f71e348c3c3ea91b4533b77809a71099
sushi <sushi@Admins-MacBook-Pro.local>
2018-02-13 15:30:27 -0800

    final commit to master

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render(){
+    return(
+      <div>
+        <p>React Launch Pad..</p>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+import App from './components/app';
+
+const htmlContainer = document.querySelector('.container');
+
+render(<App />, htmlContainer);
